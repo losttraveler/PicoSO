cb56b7e105a5f538566546219ab76f3f7a342eaa
losttraveler <rafaelangelgaib@gmail.com>
2021-10-27 17:37:29 +0200

    si

--- PicoSO/src/main.c
+++ PicoSO/src/main.c
@@ -1,9 +1,13 @@
 #include <Arduino.h>
+#include "mainAssembly.S"
 
 void setup() {
   // put your setup code here, to run once:
+  // pinMode(LED_BUILTIN,OUTPUT);
+  
 }
 
 void loop() {
+  digitalWrite(LED_BUILTIN,1);
   // put your main code here, to run repeatedly:
 }
